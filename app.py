d17f4b46b63460821302c097829772775fedfbd0
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-05-26 18:41:13 -0400

    Signed By Kenneth Rhee

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, session, redirect, escape
 import jinja2
 import os
 
 import datetime
 import pymongo
 from pymongo import MongoClient
-from functools import wraps
 
 app = Flask(__name__)
-
+app.secret_key = 'verylongkey'
 MONGO_URL = os.environ.get('MONGOHQ_URL')
 client = MongoClient(MONGO_URL)
 db = client.app25605883
 collection = db.points
 
-def check_auth(password):
-	return password == 'ihatefishsticks'
-
-def authenticate():
-	return Response('Could not verify your access level for that URL.\n'
-		'You have to login with proper credentials',401,{'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-	@wraps(f)
-	def decorated(*args, **kwargs):
-		auth = request.authorization
-		if not auth or not check_auth(auth.password):
-			return authenticate()
-		return f(*args, **kwargs)
-	return decorated
-	
 @app.route('/')
 def index():
 	#render_template will render the index.html found in the template folder
@@ -66,9 +49,22 @@ def change():
 		return redirect('/admin')
 	return redirect('/admin')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		session['username'] = request.form['username']
+		return redirect('/admin')
+	return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+	session.pop('ihatefishsticks', None)
+	return redirect('/admin')
+
 @app.route('/admin', methods=['GET','POST'])
-@requires_auth
 def admin():
+	if not 'ihatefishsticks' in session:
+		return redirect('/login')
 	if request.method == 'POST':
 		team=request.form['team']
 		event=request.form['event']
