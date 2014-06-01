8cf6ddf6ac58443885d26dc0d0682333e230e8ad
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-06-01 00:08:11 -0400

    Signed By Kenneth Rhee

--- app.py
+++ app.py
@@ -72,7 +72,8 @@ def change():
 
 @app.route('/table')
 def table():
-	return jsonify(html=collection.find())
+	template = get_template('table.html').render(events=collection.find().sort('_id',-1).limit(5))
+	return jsonify(html=template)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
