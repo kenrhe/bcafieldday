1fe1c37f51f785ab4d4c8a955dde7b31fe757510
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-06-14 14:50:25 -0400

    Signed By Kenneth Rhee

--- app.py
+++ app.py
@@ -65,8 +65,6 @@ def change():
 
 		team=request.form['team']
 		event=request.form['event']
-		if collection.find({ "event" : event}).count() == 1:
-			return render_template('admin.html', events=collection.find().sort('_id',-1), error="That event already exists! Try again with a different name.")
 		points=request.form['points']
 		if request.form['submit'] == "REMOVE":
 			collection.remove({"event" : event})
