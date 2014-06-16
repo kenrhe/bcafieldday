2a2d6b0ac0a2b9f9e7725b1ec4444ccce9d4e6c3
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-06-15 22:27:59 -0400

    Signed By Kenneth Rhee

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ statistics = db.statistics
 @app.route('/')
 def index():
 	viewCount = statistics.find({ "_id" : "539e5433dbd18c85a6000061" })
-	updatedCount = parseInt(viewCount['viewCount'])+1
+	updatedCount = int(viewCount['viewCount'])+1
 	statistics.update({ "_id" : ObjectId("539e5433dbd18c85a6000061") }, { "$set" : { "viewCount": updatedCount }}, upsert=False)
 
 	green = 0
